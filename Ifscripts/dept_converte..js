853c7fe7ee863b986fbdbb8e97154dbca3105d23
Michael JeanBaptiste <michaeljean3456@gmail.com>
2023-04-27 13:33:36 -0400

    page 3-20

---
+++ Ifscripts/dept_converte..js
@@ -0,0 +1,25 @@
+let departmentcode = 12;
+let departmentname;
+
+switch (departmentcode) {
+    case 1: 
+    departmentname = "Marketing";
+    break;
+    case 5: 
+    departmentname = "Human Resources";
+    break;
+    case 10: 
+    departmentname = "Accounting";
+    break;
+    case 12: 
+    departmentname = "Legal";
+    break;
+    case 18: 
+    departmentname = "IT";
+    break;
+    case 20: 
+    departmentname = "Customer Relations";
+    break;
+}
+
+console.log(departmentname)

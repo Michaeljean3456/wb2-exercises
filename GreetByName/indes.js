693f4b2b4f4f36b0eda27d4dbbcb5341ce36f5d3
Michael JeanBaptiste <michaeljean3456@gmail.com>
2023-05-04 23:22:15 -0400

    exercise done

---
+++ GreetByName/indes.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const months = ["January", "February", "March", "April",
+"May", "June", "July", "August", "September",
+"October", "November", "December"];
+let d = new Date();
+let monthName = months[d.getMonth("July")];
+console.log(monthname);
